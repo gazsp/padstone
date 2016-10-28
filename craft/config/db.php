5feee7db93437ee366f9efa5fdaa5793e85e3617
Gary Pearman <gpearman@gmail.com>
2016-10-28 18:14:40 +0100

    Sensible config files based on hostname

--- craft/config/db.php
+++ craft/config/db.php
@@ -1,43 +1,49 @@
 <?php
 
 /**
- * DB Configuration
+ * Database Configuration
  *
- * This is an example configuration file provided by Padstone that demonstrates
- * per environment configuration.
+ * All of your system's database configuration settings go in here.
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
-$env = preg_replace('#^.*/#', '', dirname(realpath(CRAFT_BASE_PATH)));
+return [
+    'console' => [
+        'server' => '192.168.10.10',
+        'database' => 'website-name',
+        'user' => 'homestead',
+        'password' => 'password',
+    ],
 
-if (strpos(CRAFT_BASE_PATH, '/vagrant/') === 0) {
-    $env = 'dev';
-}
+    'testing.website-name' => [
+        'server' => '192.168.10.10',
+        'database' => 'website-name',
+        'user' => 'homestead',
+        'password' => 'password',
+    ],
 
-$config = [
-    'server' => 'localhost',
-    'user' => 'web',
-    'tablePrefix' => '',
+    'local.website-name' => [
+        'server' => '192.168.10.10',
+        'database' => 'website-name',
+        'user' => 'homestead',
+        'password' => 'password',
+    ],
+
+    'website-name.co.uk' => [
+        'server' => '127.0.0.1',
+        'database' => 'website-name',
+        'user' => 'root',
+        'password' => 'root',
+    ],
+
+    'staging.website-name.co.uk' => [
+        'server' => '127.0.0.1',
+        'database' => 'website-name',
+        'user' => 'root',
+        'password' => 'root',
+    ],
+
+    '*' => [
+        'tablePrefix' => 'craft',
+    ]
 ];
-
-switch ($env) {
-    case 'dev':
-        $config = array_merge($config, [
-            'database' => 'dev_example_com',
-        ]);
-        break;
-    case 'stage':
-        $config = array_merge($config, [
-            'database' => 'stage_example_com',
-        ]);
-        break;
-    case 'prod':
-        $config = array_merge($config, [
-            'database' => 'example_com',
-        ]);
-        break;
-    default:
-        die("Unfortunately, the server is misconfigured. Please review the configuration in config/db.php.");
-}
-
-return $config;
--- craft/config/general.php
+++ craft/config/general.php
@@ -3,51 +3,64 @@
 /**
  * General Configuration
  *
- * This is an example configuration file provided by Padstone that demonstrates
- * per environment configuration.
+ * All of your system's general configuration settings go in here.
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
-$env = preg_replace('#^.*/#', '', dirname(realpath(CRAFT_BASE_PATH)));
+return [
+    '*' => [
+        'autoLoginAfterAccountActivation' => true,
+        'overridePhpSessionLocation' => true,
+        'omitScriptNameInUrls' => true,
+        'defaultSearchTermOptions' => [
+            'subLeft' => true,
+            'subRight' => true,
+        ],
+        'setPasswordPath' => 'user/reset-password',
+        'env' => 'dev',
+        'tokenParam' => 't'
+    ],
 
-if (strpos(CRAFT_BASE_PATH, '/vagrant/') === 0) {
-    $env = 'dev';
-}
+    'local.website-name' => [
+        'devMode' => true,
+        'siteUrl' => 'http://local.website-name/',
+    ],
 
-$config = [
-    'generateTransformsBeforePageLoad' => true,
-    'maxUploadFileSize' => 104857600,
-    'omitScriptNameInUrls' => true,
+    'testing.website-name' => [
+        'siteUrl' => 'http://testing.website-name/',
+    ],
+
+    'website-name.co.uk' => [
+        'siteUrl' => 'http://website-name.co.uk/',
+        'devMode' => false,
+        'patrol' => [
+            'primaryDomain'   => '*',
+            'forceSsl'        => false,
+            'restrictedAreas' => [
+            ],
+            'maintenanceUrl'  => '/offline.html',
+            'authorizedIps'   => [
+                '77.101.79.140',
+                '86.27.201.244',
+                // PayPal IPN servers
+                '173.0.81.*',
+                '173.0.82.*'
+            ],
+            'limitCpAccessTo' => [],
+            'enableCpTab'     => false,
+            'pluginAlias'     => 'Patrol'
+        ],
+        'env' => 'live'
+    ],
+
+    'staging.website-name.co.uk' => [
+        'siteUrl' => 'http://staging.website-name.co.uk/',
+        'devMode' => true,
+        'patrol' => [
+            'primaryDomain'   => '*',
+            'maintenanceMode' => true,
+            'maintenanceUrl'  => '/redirect.html'
+        ],
+        'env' => 'dev'
+    ]
 ];
-
-switch ($env) {
-    case 'dev':
-        $config = array_merge($config, [
-            'siteUrl' => "http://dev.example.com",
-            'devMode' => true,
-            'testToEmailAddress' => 'test+example.com@test.com',
-            'useCompressedJs' => false,
-            'cache' => false,
-        ]);
-        break;
-
-    case 'stage':
-        $config = array_merge($config, [
-            'siteUrl' => 'http://stage.example.com',
-            'useCompressedJs' => false,
-            'cache' => true,
-        ]);
-        break;
-
-    case 'prod':
-        $config = array_merge($config, [
-            'siteUrl' => 'https://example.com',
-            'cache' => true,
-        ]);
-        break;
-
-    default:
-        die("Unfortunately, the server is misconfigured. Please review the configuration in config/general.php.");
-}
-
-return $config;
